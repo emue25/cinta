a343f41c56d2f153dac639612c99ce5109905032
brantbell <31989675+brantbell@users.noreply.github.com>
2018-07-28 10:51:59 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -24,21 +24,6 @@ else
 	source="http://anekascript.anekavps.us"
 fi
 
-# go to root
-cd
-
-# check registered ip
-#wget -q -O IP $source/IP.txt
-#if ! grep -w -q $MYIP IP; then
-#	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
-#	if [[ $vps = "zvur" ]]; then
-#		echo "Hubungi: Yuri Bhuana (fb.com/youree82 atau 0858 1500 2021)"
-#	else
-#		echo "Hubungi: Turut Dwi Hariyanto (fb.com/turut.dwi.hariyanto atau 085735313729)"
-#	fi
-#	rm -f /root/IP
-#	exit
-#fi
 
 # disable ipv6
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
