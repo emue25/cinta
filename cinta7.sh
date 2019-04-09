477e75bdd1e20b65bf37612b9e0d5da9ed8a0468
brantbell <31989675+brantbell@users.noreply.github.com>
2019-04-09 09:28:01 +0800

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -79,9 +79,11 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
 # set repo
-wget -O /etc/apt/sources.list $source/debian7/sources.list.debian7
+wget -O /etc/apt/sources.list $source/debian7/sources.list.debian9
 wget http://www.dotdeb.org/dotdeb.gpg
+wget http://www.webmin.com/jcameron-key.asc
 cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 
 # remove unused
 sudo apt-get -y --purge remove samba*;
